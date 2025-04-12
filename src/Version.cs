918b9a422554dc314d8d7189bee67ee193a69b8e
Kalle <kalle@kandru.de>
2025-04-12 21:46:16 +0200

    bump version to 1.0.5

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace QuakeSounds
 {
     public partial class QuakeSounds
     {
-        public override string ModuleVersion => "1.0.4";
+        public override string ModuleVersion => "1.0.5";
     }
 }
